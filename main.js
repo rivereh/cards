08c6a5ca9c2985960951b7419a3b39f5e312a992
River <riverehill@gmail.com>
2022-09-13 17:28:59 -0700

    Add new card style

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const cardContainer = document.querySelector('.card-container');
+const cardContainer = document.querySelector('.cards');
 const createCardContainer = document.querySelector('.create-card-container');
 const questionTextArea = document.querySelector('#question');
 const answerTextArea = document.querySelector('#answer');
@@ -31,7 +31,7 @@ function addCard() {
 }
 
 function cardMaker(cardInfo) {
-    let div = document.createElement('div');
+    let card = document.createElement('div');
     let question = document.createElement('h2');
     let answer = document.createElement('h2');
     let deleteButton = document.createElement('p');
@@ -46,31 +46,31 @@ function cardMaker(cardInfo) {
         cardArray.splice(cardIndex, 1);
 
         localStorage.setItem('items', JSON.stringify(cardArray));
-        cardContainer.removeChild(div);
+        cardContainer.removeChild(card);
     });
 
-    div.appendChild(deleteButton);
+    card.appendChild(deleteButton);
 
-    div.className = 'card';
+    card.className = 'card';
     question.classList.add('card-question');
     answer.classList.add('card-answer');
     question.textContent = cardInfo.question;
     answer.textContent = cardInfo.answer;
     answer.style.display = 'none';
-    div.appendChild(question);
-    div.appendChild(answer);
+    card.appendChild(question);
+    card.appendChild(answer);
     
 
     question.setAttribute('contenteditable', 'true');
 
-    div.addEventListener('click', () => {
+    card.addEventListener('click', () => {
         if (answer.style.display == 'none')
             answer.style.display = 'block';
         else
             answer.style.display = 'none';
     });
 
-    cardContainer.appendChild(div);
+    cardContainer.appendChild(card);
 }
 
 function toggleCreateCardContainer() {
